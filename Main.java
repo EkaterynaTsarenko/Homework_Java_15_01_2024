fc00e0cd0214ca9158d6a24b848de31a007f0ea8
EkaterynaTsarenko <129986738+EkaterynaTsarenko@users.noreply.github.com>
2024-01-22 21:54:01 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,73 @@
 public class Main {
     public static void main(String[] args) {
 
-        //3
-        //000 - изображение радуги
-        //001 - красный цвет
-        //010 - оранжевый цвет
-        //011 - желтый цвет
-        //111 - зеленый цвет
-        //110 - голубой цвет
-        //100 - синий цвет
-        //010 - фиолетовый цвет
-        //101 - изображение облака
+      //int - целочисленное значение в коробке(переменной)
+        int box1 = 17000;
+        int box2 = 750;
+     System.out.println(box1 + box2);
+
+     int art1 = -21;
+     int art2 = 100000;
+     System.out.println(art2 - art1);
+
+       //String - любое текстовое сообщение в коробке(переменной)
+        String text = "Это мой текст в коробке!";
+        System.out.println(text);
+
+        //Short - целочисленный тип, но содержит значение меньшего размера
+        short shortFile = 30000;
+        System.out.println(shortFile);
+
+        //byte - целочисленный тип
+        byte byteFile = 100;
+        System.out.println(byteFile);
+
+        //long - целочисленный тип
+        long longFile = 1000000000;
+        System.out.println(longFile);
+
+        //double - тип для дробных чисел
+        double doubleFile = 0.33;
+        System.out.println(doubleFile);
+
+        //char - символьный тип
+         char charFile = 'Э';
+        System.out.println(charFile);
+
+        //Число 1
+        //Число 2
+        //Арифм. операция: +, -, *, /
+        //Число 1 + Число 2 = какой-то результат
+
+        int number1 = 60;
+        int number2 = 2;
+        int result = number1 + number2;
+        System.out.println(result);
+
+
+        int result2 = number1-number2;
+        System.out.println(result2);
+
+        int result3 = number2 * number1;
+        System.out.println(result3);
+
+        int result4 = number1 / number2;
+        System.out.println(result4);
         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
